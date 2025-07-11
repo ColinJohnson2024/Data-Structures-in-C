ace611d5b59eeab98f20da7587ceadcd2b754da0
Colin Johnson <colinjohnson@Colin-Laptop.localdomain>
2025-07-11 17:20:21 -0500

    Added initial binary tree header file

---
+++ include/binary_tree.h
@@ -0,0 +1,25 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Tree {
+    struct Node* root;
+} Tree;
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Tree* create_tree();
+int insert_root(Tree* tree, int data);
+int delete_tree(Tree** tree);
+Node* create_node(int data);
+int insert_left(Node* parent, int data);
+int insert_right(Node* parent, int data);
+int delete_node(Node* node);
+
+#endif
